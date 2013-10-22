98b4832b32cfa480f3200b12141f0bcef4975a07
Mosky <mosky.tw@gmail.com>
2013-10-23 02:27:24 +0800

    shorter

--- test_fcntl.c
+++ test_fcntl.c
@@ -8,10 +8,9 @@ int main(int argc, char* argv[]) {
     puts("It is annoying, right? Press 'y' to exit.");
 
     termios_disable_canonical_echo();
-    while (1) {
+    while (getchar() != 'y') {
         puts("Hi, there!");
         usleep(500000); // 0.5s
-        if (getchar() == 'y') break;
     }
     termios_restore();
 
