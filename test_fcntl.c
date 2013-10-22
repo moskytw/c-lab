f3f0312f66c28701a5256cb67ef90cf6ef69f7c0
Mosky <mosky.tw@gmail.com>
2013-10-23 02:30:33 +0800

    set stdin nonblocking

--- test_fcntl.c
+++ test_fcntl.c
@@ -1,9 +1,11 @@
 #include "termios_util.h"
 #include <fcntl.h>
-#include <unistd.h> // usleep
+#include <unistd.h> // usleep, STDIN_FILENO
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
+
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 
     puts("It is annoying, right? Press 'y' to exit.");
 
