e1e615d192e86ead31799716a6646ae28ea8e110
Mosky <mosky.tw@gmail.com>
2013-10-25 00:08:49 +0800

    alarm is also useful!

---
+++ test_alarm.c
@@ -0,0 +1,27 @@
+#include "termios_util.h"
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h> // STDIN_FILENO, ualarm
+#include <stdio.h>
+
+void my_sigalrm_handler(int signal) {
+    puts("Hi, there!");
+}
+
+int main(int argc, char* argv[]) {
+
+    if (fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK) == -1) return 1;
+
+    signal(SIGALRM, my_sigalrm_handler);
+    ualarm(500000, 500000);
+
+    puts("It is annoying, right? Press 'y' to exit.");
+
+    termios_disable_canonical_echo();
+    while (getchar() != 'y');
+    termios_restore();
+
+    puts("Exit!");
+
+    return 0;
+}
