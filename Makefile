f9ef876a8757841997730945368aeaf3cfee7836
Mosky <mosky.tw@gmail.com>
2013-11-16 23:33:15 +0800

    finished the draft of test_socket

--- Makefile
+++ Makefile
@@ -11,13 +11,17 @@ CFLAGS = -O3 -Wall -I src/lib
 default: $(foreach PATH,$(wildcard src/bin/*.c),$(patsubst src/bin/%.c,bin/%,$(PATH)))
 
 # Define the dependences:
+#
+bin/test_ansi_esc_seq: src/bin/test_ansi_esc_seq.c obj/ansi_esc_seq.o
+bin/test_termios_util: src/bin/test_termios_util.c obj/termios_util.o
+
 bin/test_alarm: src/bin/test_alarm.c obj/termios_util.o
 bin/test_fcntl: src/bin/test_fcntl.c obj/termios_util.o
 bin/test_fcntl_async: src/bin/test_fcntl_async.c obj/termios_util.o
 bin/test_fork: src/bin/test_fork.c obj/termios_util.o
 bin/test_pthread: src/bin/test_pthread.c obj/termios_util.o
-bin/test_ansi_esc_seq: src/bin/test_ansi_esc_seq.c obj/ansi_esc_seq.o
-bin/test_termios_util: src/bin/test_termios_util.c obj/termios_util.o
+
+bin/test_socket: src/bin/test_socket.c
 
 # It is a shortcut for the above targets.
 bin/%:
---
+++ src/bin/test_socket.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h> // read, write, close
+#include <sys/socket.h>
+#include <arpa/inet.h> // sockaddr_in, inet_addr, htons
+
+int main(int argc, char* argv[]) {
+
+    int socket_desc;
+    socket_desc = socket(PF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        fprintf(stderr, "Could not create socket.\n");
+        return 1;
+    }
+    printf("A socket is created. The socket descriptor is %d.\n", socket_desc);
+
+    char* addrstr = "127.0.0.1";
+    int port = 5000;
+    struct sockaddr_in server_sin = {0};
+    server_sin.sin_addr.s_addr = inet_addr(addrstr);
+    server_sin.sin_port = htons(port);
+
+    if (connect(socket_desc, (struct sockaddr*) &server_sin, sizeof(server_sin)) == -1) {
+        fprintf(stderr, "Could not make connection to %s:%d\n", addrstr, port);
+        return 1;
+    }
+    printf("Connected to %s:%d.\n", addrstr, port);
+
+    close(socket_desc);
+
+    return 0;
+}
