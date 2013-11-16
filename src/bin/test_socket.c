9292ef8d38b75a66431a66c2a4c169c4106bbc77
Mosky <mosky.tw@gmail.com>
2013-11-17 01:25:04 +0800

    show the error str

--- src/bin/test_socket.c
+++ src/bin/test_socket.c
@@ -1,7 +1,9 @@
 #include <sys/socket.h>
+#include <errno.h>     // errno
+#include <string.h>    // strerror
 #include <stdio.h>
+#include <stdlib.h>    // atoi
 #include <arpa/inet.h> // sockaddr_in, inet_addr, htons
-#include <stdlib.h> // atoi
 
 int main(int argc, char* argv[]) {
 
@@ -9,7 +11,7 @@ int main(int argc, char* argv[]) {
     int socket_desc;
     socket_desc = socket(PF_INET, SOCK_STREAM, 0);
     if (socket_desc == -1) {
-        fprintf(stderr, "Could not create socket.\n");
+        fprintf(stderr, "Could not create socket: %s.\n", strerror(errno));
         return 1;
     }
     printf("The socket descriptor is %d.\n", socket_desc);
@@ -25,7 +27,7 @@ int main(int argc, char* argv[]) {
 
     // Connect to server:
     if (connect(socket_desc, (struct sockaddr*) &server_sin, sizeof server_sin) == -1) {
-        fprintf(stderr, "Could not make connection to %s:%d\n", addrstr, port);
+        fprintf(stderr, "Could not make connection to %s port %d: %s.\n", addrstr, port, strerror(errno));
         return 1;
     }
     printf("Connected to %s:%d.\n", addrstr, port);
@@ -33,7 +35,7 @@ int main(int argc, char* argv[]) {
     // Send data to server:
     char data[] = "GET / HTTP/1.1\r\n\r\n";
     if (send(socket_desc, data, sizeof data, 0) == -1) {
-        fprintf(stderr, "Could not send data.\n");
+        fprintf(stderr, "Could not send data: %s.\n", strerror(errno));
         return 1;
     }
     puts("Data sent.");
@@ -50,7 +52,7 @@ int main(int argc, char* argv[]) {
 
     // Shutdown socket:
     if (shutdown(socket_desc, SHUT_RDWR) == -1) {
-        fprintf(stderr, "Could not shut socket down.\n");
+        fprintf(stderr, "Could not shut socket down: %s.\n", strerror(errno));
         return 1;
     }
     puts("The socket is shut down.");
