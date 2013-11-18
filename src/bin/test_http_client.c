a8bc11754de97e4e8e196d99504939b754cddd8e
Mosky <mosky.tw@gmail.com>
2013-11-18 23:55:58 +0800

    abstracted the routines for test_http_client.c

--- src/bin/test_http_client.c
+++ src/bin/test_http_client.c
@@ -16,44 +16,59 @@ int my_close(int file_desc) {
     return return_val;
 }
 
-int main(int argc, char* argv[]) {
-
-    // Open socket:
+int my_socket_stream() {
     int socket_desc = socket(PF_INET, SOCK_STREAM, 0);
     if (socket_desc == -1) {
         fprintf(stderr, "Could not create socket: %s.\n", strerror(errno));
         exit(1);
     }
     puts("The socket is opened.");
+    return socket_desc;
+}
 
-    // Set remote address:
-    char* addr_str = "127.0.0.1";
-    int port = 5000;
-    struct sockaddr_in remote_addr = {0};
-    if (argc >= 2) port = atoi(argv[1]);
-    if (argc >= 3) addr_str = argv[2];
-    remote_addr.sin_family = PF_INET;
-    inet_pton(PF_INET, addr_str, &(remote_addr.sin_addr));
-    remote_addr.sin_port = htons(port);
+void my_sockaddr_set_addr(struct sockaddr_in* addr_ptr, char* addr_str) {
+    addr_ptr->sin_family = PF_INET;
+    inet_pton(PF_INET, addr_str, &(addr_ptr->sin_addr));
+}
 
-    // Connect to remote:
-    if (connect(socket_desc, (struct sockaddr*) &remote_addr, sizeof remote_addr) == -1) {
+void my_sockaddr_set_port(struct sockaddr_in* addr_ptr, int port) {
+    addr_ptr->sin_port = htons(port);
+}
+
+void my_sockaddr_get_addr(struct sockaddr_in* addr_ptr, char* addr_str, int addr_str_size) {
+    inet_ntop(PF_INET, &(addr_ptr->sin_addr), addr_str, addr_str_size);
+}
+
+void my_sockaddr_get_port(struct sockaddr_in* addr_ptr, int* port_ptr) {
+    *port_ptr = ntohs(addr_ptr->sin_port);
+}
+
+void my_connect(int socket_desc, struct sockaddr_in* addr_ptr) {
+
+    char addr_str[INET_ADDRSTRLEN];
+    my_sockaddr_get_addr(addr_ptr, addr_str, sizeof addr_str);
+    int port;
+    my_sockaddr_get_port(addr_ptr, &port);
+
+    if (connect(socket_desc, (struct sockaddr*) addr_ptr, sizeof *addr_ptr) == -1) {
         fprintf(stderr, "Could not connect to %s on port %d: %s.\n", addr_str, port, strerror(errno));
         my_close(socket_desc);
         exit(1);
     }
     printf("Connected to %s:%d.\n", addr_str, port);
 
-    // Send data to remote:
-    char data[] = "GET / HTTP/1.1\r\n\r\n";
-    if (write(socket_desc, data, sizeof data) == -1) {
+}
+
+void my_send(int socket_desc, char* data, int data_size) {
+    if (write(socket_desc, data, data_size) == -1) {
         fprintf(stderr, "Could not send data: %s.\n", strerror(errno));
         my_close(socket_desc);
         exit(1);
     }
     puts("Sent data.");
+}
 
-    // Receive data from remote:
+void my_receive(int socket_desc) {
     int read_size;
     char buffer[1024];
     puts("--- Data received ---");
@@ -67,6 +82,33 @@ int main(int argc, char* argv[]) {
     }
     if (buffer[read_size-1] != '\n') puts("");
     puts("--- End ---");
+}
+
+int main(int argc, char* argv[]) {
+
+    // Open stream socket:
+    int socket_desc = my_socket_stream();
+
+    // Get the port and addr settings from user:
+    int port = 5000;
+    if (argc >= 2) port = atoi(argv[1]);
+    char* addr_str = "127.0.0.1";
+    if (argc >= 3) addr_str = argv[2];
+
+    // Set remote address:
+    struct sockaddr_in remote_addr;
+    my_sockaddr_set_port(&remote_addr, port);
+    my_sockaddr_set_addr(&remote_addr, addr_str);
+
+    // Connect to remote:
+    my_connect(socket_desc, &remote_addr);
+
+    // Send data to remote:
+    char data[] = "GET / HTTP/1.1\r\n\r\n";
+    my_send(socket_desc, data, sizeof data);
+
+    // Receive data from remote:
+    my_receive(socket_desc);
 
     // Close the socket:
     my_close(socket_desc);
