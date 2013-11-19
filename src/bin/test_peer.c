9e01fc792d33e5e429384efdb7d0b10b4a8f786c
Mosky <mosky.tw@gmail.com>
2013-11-19 23:15:24 +0800

    finished the broadcast and discover mechanisms

--- src/bin/test_peer.c
+++ src/bin/test_peer.c
@@ -8,44 +8,44 @@
 #include <string.h>    // strerror
 #include <unistd.h>    // STDOUT_FILENO, write, read
 
-// TODO: Define the control message.
-
 const int broadcast_addr_ptrs_size = 10;
 struct sockaddr_in* broadcast_addr_ptrs[broadcast_addr_ptrs_size] = {0};
+int broadcast_addr_ptrs_empty_idx = 0;
 pthread_mutex_t broadcast_addr_ptrs_mutex;
 
 void my_broadcast_addr_ptrs_add(struct sockaddr_in* remote_addr_ptr) {
 
+    pthread_mutex_lock(&broadcast_addr_ptrs_mutex);
+
+    if (broadcast_addr_ptrs_empty_idx >= broadcast_addr_ptrs_size) return;
+
     int i;
-    int empty_idx = -1;
-
     for (i = 0; i < broadcast_addr_ptrs_size; i++) {
-
         // do noting if this addr is existent
-        // TODO: Use binary tree or hash table to do it.
         if (broadcast_addr_ptrs[i] != NULL &&
-            !memcmp(broadcast_addr_ptrs[i], remote_addr_ptr, sizeof (struct sockaddr_in))) {
+            !memcmp(broadcast_addr_ptrs[i], remote_addr_ptr, sizeof *remote_addr_ptr)) {
             return;
         }
-
-        // find an empty index
-        if (empty_idx == -1 && broadcast_addr_ptrs[i] == NULL) {
-            empty_idx = i;
-        }
-
     }
 
-    if (empty_idx != -1) {
-        broadcast_addr_ptrs[empty_idx] = malloc(sizeof (struct sockaddr_in));
-        *broadcast_addr_ptrs[empty_idx] = *remote_addr_ptr;
-        puts("It is a new remote!");
+    broadcast_addr_ptrs[broadcast_addr_ptrs_empty_idx] = malloc(sizeof *remote_addr_ptr);
+    *broadcast_addr_ptrs[broadcast_addr_ptrs_empty_idx++] = *remote_addr_ptr;
+    puts("A new peer is added to list!");
+
+    pthread_mutex_unlock(&broadcast_addr_ptrs_mutex);
+}
+
+void my_broadcast_except(int bind_socket_desc, char* data_ptr, int data_size, struct sockaddr_in* excepted_addr_ptr) {
+    int i;
+    for (i = 0; i < broadcast_addr_ptrs_size; i++) {
+        if (broadcast_addr_ptrs[i] == NULL) continue;
+        if (excepted_addr_ptr != NULL && !memcmp(broadcast_addr_ptrs[i], excepted_addr_ptr, sizeof *excepted_addr_ptr)) continue;
+        socket_util_sendto(bind_socket_desc, broadcast_addr_ptrs[i], data_ptr, data_size);
     }
 }
 
-void my_broadcast_addr_ptrs_erase(int i) {
-    free(broadcast_addr_ptrs[i]);
-    broadcast_addr_ptrs[i] = NULL;
-}
+#define HI "\0HI"
+#define HI_ACK "\0HI_ACK"
 
 int bind_socket_desc;
 
@@ -63,20 +63,26 @@ void* my_receiver() {
             pthread_exit(NULL);
         }
 
-        pthread_mutex_lock(&broadcast_addr_ptrs_mutex);
-        my_broadcast_addr_ptrs_add(&remote_addr);
-        pthread_mutex_unlock(&broadcast_addr_ptrs_mutex);
+        if (buffer[0] == '\0') {
+            // If is HI or HI_ACK:
+            if (!memcmp(buffer, HI, (sizeof HI)-1)) {
+                my_broadcast_addr_ptrs_add(&remote_addr);
+                // If is not HI_ACK:
+                if (memcmp(buffer, HI_ACK, sizeof HI_ACK)) {
+                    socket_util_sendto(bind_socket_desc, &remote_addr, HI_ACK, sizeof HI_ACK);
+                }
+            }
+            continue;
+        }
 
         char remote_addr_str[INET_ADDRSTRLEN];
         int remote_port;
         socket_util_sockaddr_get_addr(&remote_addr, remote_addr_str, sizeof remote_addr_str);
         socket_util_sockaddr_get_port(&remote_addr, &remote_port);
+        buffer[read_size] = 0;
+        printf("[%s:%d] %s", remote_addr_str, remote_port, buffer);
 
-        // TODO: This message should be sent to all broadcast addrs except this
-        // remote.
-        printf("[%s:%d] ", remote_addr_str, remote_port);
-        fflush(stdout);
-        write(STDOUT_FILENO, buffer, read_size);
+        my_broadcast_except(bind_socket_desc, buffer, read_size, &remote_addr);
 
     }
 
@@ -85,17 +91,11 @@ void* my_receiver() {
 
 int main(int argc, char* argv[]) {
 
-    // Get the port and addr settings:
-    int bind_port = 5000;
-    if (argc >= 2) bind_port = atoi(argv[1]);
-    char* bind_addr_str_ptr = "0.0.0.0";
-    if (argc >= 3) bind_addr_str_ptr = argv[2];
-
     // Open a socket for binding the address:
     bind_socket_desc = socket_util_socket_datagram();
 
     // Bind the socket with the address:
-    socket_util_bind_addr_port_retry(bind_socket_desc, bind_addr_str_ptr, bind_port, 3);
+    int bind_port = socket_util_bind_addr_port_retry(bind_socket_desc, "0.0.0.0", 5000, 3);
 
     // Create thread to receive data in background:
     pthread_t my_receiver_thread;
@@ -104,14 +104,17 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    // Try to discover a peer:
+    struct sockaddr_in remote_addr = {0};
+    socket_util_sockaddr_set_addr(&remote_addr, "127.0.0.1");
+    socket_util_sockaddr_set_port(&remote_addr, (bind_port == 5000) ? 5001 : 5000);
+    socket_util_sendto(bind_socket_desc, &remote_addr, HI, sizeof HI);
+
     // Accept message from user:
     while (1) {
         char buffer[1024];
         fgets(buffer, sizeof buffer, stdin);
-        // TODO: send message to all broadcast addrs
-        if (!strncmp(buffer, "exit", 4)) {
-            exit(0);
-        }
+        my_broadcast_except(bind_socket_desc, buffer, strlen(buffer), NULL);
     }
 
     // Close bind socket:
