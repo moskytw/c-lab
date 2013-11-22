3c0e0d51c9087adcce21925f0edab217b90a4118
Mosky <mosky.tw@gmail.com>
2013-11-22 23:49:55 +0800

    decoupled with socket_util

--- src/bin/test_peer.c
+++ src/bin/test_peer.c
@@ -1,4 +1,3 @@
-#include "socket_util.h"
 #include <stdio.h>
 #include <stdlib.h>    // atoi, exit, malloc, free
 #include <pthread.h>
@@ -8,12 +7,45 @@
 #include <string.h>    // strerror
 #include <unistd.h>    // STDOUT_FILENO, write, read
 
+void my_exit_if_errno(char* message_tmpl) {
+    if (errno == 0) return;
+    fprintf(stderr, message_tmpl, strerror(errno));
+    exit(1);
+}
+
+void my_sockaddr_set(struct sockaddr* addr_ptr, char* addr_str_ptr, int port) {
+
+    struct sockaddr_in* addr_in_ptr = (struct sockaddr_in*) addr_ptr;
+
+    if (addr_str_ptr != NULL) {
+        addr_in_ptr->sin_family = PF_INET;
+        inet_pton(PF_INET, addr_str_ptr, &(addr_in_ptr->sin_addr));
+    }
+
+    if (port != -1) {
+        addr_in_ptr->sin_port = htons(port);
+    }
+}
+
+void my_sockaddr_get(struct sockaddr* addr_ptr, char* addr_str_ptr, int addr_str_size, int* port_ptr) {
+
+    struct sockaddr_in* addr_in_ptr = (struct sockaddr_in*) addr_ptr;
+
+    if (addr_str_ptr != NULL) {
+        inet_ntop(PF_INET, &(addr_in_ptr->sin_addr), addr_str_ptr, addr_str_size);
+    }
+
+    if (port_ptr != NULL) {
+        *port_ptr = ntohs(addr_in_ptr->sin_port);
+    }
+}
+
 const int broadcast_addr_ptrs_size = 10;
-struct sockaddr_in* broadcast_addr_ptrs[broadcast_addr_ptrs_size] = {0};
+struct sockaddr* broadcast_addr_ptrs[broadcast_addr_ptrs_size] = {0};
 int broadcast_addr_ptrs_empty_idx = 0;
 pthread_mutex_t broadcast_addr_ptrs_mutex;
 
-void my_broadcast_addr_ptrs_add(struct sockaddr_in* remote_addr_ptr) {
+void my_broadcast_addr_ptrs_add(struct sockaddr* remote_addr_ptr) {
 
     pthread_mutex_lock(&broadcast_addr_ptrs_mutex);
 
@@ -35,12 +67,12 @@ void my_broadcast_addr_ptrs_add(struct sockaddr_in* remote_addr_ptr) {
     pthread_mutex_unlock(&broadcast_addr_ptrs_mutex);
 }
 
-void my_broadcast_except(int bind_socket_desc, char* data_ptr, int data_size, struct sockaddr_in* excepted_addr_ptr) {
+void my_broadcast_except(int bind_socket_desc, char* data_ptr, int data_size, struct sockaddr* excepted_addr_ptr) {
     int i;
     for (i = 0; i < broadcast_addr_ptrs_size; i++) {
         if (broadcast_addr_ptrs[i] == NULL) continue;
         if (excepted_addr_ptr != NULL && !memcmp(broadcast_addr_ptrs[i], excepted_addr_ptr, sizeof *excepted_addr_ptr)) continue;
-        socket_util_sendto(bind_socket_desc, broadcast_addr_ptrs[i], data_ptr, data_size);
+        sendto(bind_socket_desc, data_ptr, data_size, 0, broadcast_addr_ptrs[i], sizeof *broadcast_addr_ptrs[i]);
     }
 }
 
@@ -53,15 +85,12 @@ void* my_receiver() {
 
     int read_size;
     char buffer[1024];
-    struct sockaddr_in remote_addr = {0};
+    struct sockaddr remote_addr = {0};
     int remote_addr_size = sizeof remote_addr;
 
-    while ((read_size = recvfrom(bind_socket_desc, buffer, sizeof buffer, 0, (struct sockaddr*) &remote_addr, (socklen_t*) &remote_addr_size))) {
+    while ((read_size = recvfrom(bind_socket_desc, buffer, sizeof buffer, 0, &remote_addr, (socklen_t*) &remote_addr_size))) {
 
-        if (read_size == -1) {
-            fprintf(stderr, "Could not read data: %s.\n", strerror(errno));
-            pthread_exit(NULL);
-        }
+        my_exit_if_errno("Could not read data: %s.\n");
 
         if (buffer[0] == '\0') {
             // If is HI or HI_ACK:
@@ -69,7 +98,7 @@ void* my_receiver() {
                 my_broadcast_addr_ptrs_add(&remote_addr);
                 // If is not HI_ACK:
                 if (memcmp(buffer, HI_ACK, sizeof HI_ACK)) {
-                    socket_util_sendto(bind_socket_desc, &remote_addr, HI_ACK, sizeof HI_ACK);
+                    sendto(bind_socket_desc, HI_ACK, sizeof HI_ACK, 0, &remote_addr, sizeof remote_addr);
                 }
             }
             continue;
@@ -77,8 +106,7 @@ void* my_receiver() {
 
         char remote_addr_str[INET_ADDRSTRLEN];
         int remote_port;
-        socket_util_sockaddr_get_addr(&remote_addr, remote_addr_str, sizeof remote_addr_str);
-        socket_util_sockaddr_get_port(&remote_addr, &remote_port);
+        my_sockaddr_get(&remote_addr, remote_addr_str, sizeof remote_addr_str, &remote_port);
         buffer[read_size] = 0;
         printf("[%s:%d] %s", remote_addr_str, remote_port, buffer);
 
@@ -92,23 +120,43 @@ void* my_receiver() {
 int main(int argc, char* argv[]) {
 
     // Open a socket for binding the address:
-    bind_socket_desc = socket_util_socket_datagram();
+    bind_socket_desc = socket(PF_INET, SOCK_DGRAM, 0);
+    my_exit_if_errno("Could not create stream socket: %s.\n");
 
     // Bind the socket with the address:
-    int bind_port = socket_util_bind_addr_port_retry(bind_socket_desc, "0.0.0.0", 5000, 3);
+
+    char bind_addr_str[] = "0.0.0.0";
+    int bind_port = 5000;
+    struct sockaddr bind_addr = {0};
+    my_sockaddr_set(&bind_addr, bind_addr_str, bind_port);
+
+    int retry_limit = 3;
+    while (retry_limit--) {
+        if (bind(bind_socket_desc, &bind_addr, (socklen_t) sizeof bind_addr) == 0) break;
+        if (errno == EADDRINUSE) {
+            errno = 0;
+            printf("The address %s port %d is in use. Try next port.\n",
+                bind_addr_str, bind_port
+            );
+            my_sockaddr_set(&bind_addr, NULL, ++bind_port);
+            continue;
+        }
+        my_exit_if_errno("Could not bind: %s.\n");
+    }
+    my_exit_if_errno("Could not find a unused port: %s.\n");
+    printf("Bound to %s:%d.\n", bind_addr_str, bind_port);
 
     // Create thread to receive data in background:
     pthread_t my_receiver_thread;
     if (pthread_create(&my_receiver_thread, NULL, my_receiver, NULL)) {
-        fprintf(stderr, "Could not create thread: %s\n", strerror(errno));
+        fprintf(stderr, "Could not create thread: %s.\n", strerror(errno));
         exit(1);
     }
 
     // Try to discover a peer:
-    struct sockaddr_in remote_addr = {0};
-    socket_util_sockaddr_set_addr(&remote_addr, "127.0.0.1");
-    socket_util_sockaddr_set_port(&remote_addr, (bind_port == 5000) ? 5001 : 5000);
-    socket_util_sendto(bind_socket_desc, &remote_addr, HI, sizeof HI);
+    struct sockaddr remote_addr = {0};
+    my_sockaddr_set(&remote_addr, "127.0.0.1", (bind_port == 5000) ? 5001 : 5000);
+    sendto(bind_socket_desc, HI, sizeof HI, 0, &remote_addr, sizeof remote_addr);
 
     // Accept message from user:
     while (1) {
@@ -119,7 +167,7 @@ int main(int argc, char* argv[]) {
 
     // Close bind socket:
     printf("Closing bind socket: ");
-    socket_util_close(bind_socket_desc);
+    close(bind_socket_desc);
 
     exit(0);
 }
