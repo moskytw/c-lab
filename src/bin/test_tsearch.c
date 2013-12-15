c5507ac7943d482c144d35094089048bf3456d70
Mosky <mosky.tw@gmail.com>
2013-12-15 23:21:47 +0800

    fixed the warnings on linux

--- src/bin/test_tsearch.c
+++ src/bin/test_tsearch.c
@@ -2,6 +2,8 @@
 #include <stdlib.h> // free
 #include <search.h>
 
+#define LEN(arr) (sizeof arr / sizeof arr[0])
+
 int int_ptr_cmp(const void* a_ptr, const void* b_ptr) {
     return *(int*)a_ptr-*(int*)b_ptr;
 }
@@ -14,12 +16,11 @@ void print_node_ptr(const void* node_ptr, VISIT order, int level) {
 
 int main(int argc, char* argv[]) {
 
-    const int size = 5;
-    int values[size] = {1, 2, 3, 4, 5};
+    int values[] = {1, 2, 3, 4, 5};
     void* root = NULL;
 
     int i;
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < LEN(values); i++) {
         // This binary tree actually saves the pointer of the value.
         tsearch((void*) &values[i], &root, int_ptr_cmp);
     }
@@ -37,4 +38,6 @@ int main(int argc, char* argv[]) {
     twalk(root, print_node_ptr);
     // TODO: ensure it is enough to free this free.
     free(root);
+
+    return 0;
 }
